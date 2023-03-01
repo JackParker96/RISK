25603bf74ed6070e752af52e3bff878de4300fe6
Jack Parker <jack.parker@duke.edu>
2023-03-01 09:38:04 -0500

    Changed package name from teamX to team14 in shared, client, and server

--- gradle-skeleton/shared/src/main/java/edu/duke/ece651/team14/shared/MyName.java
+++ gradle-skeleton/shared/src/main/java/edu/duke/ece651/team14/shared/MyName.java
@@ -1,7 +1,7 @@
-package edu.duke.ece651.teamX.shared;
+package edu.duke.ece651.team14.shared;
 
 public class MyName {
   public static String getName(){
-    return "teamX";
+    return "team14";
   }
 }
