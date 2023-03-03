a934e79f18649709f9c11ccefbe394ecd66cb476
Jack Parker <jack.parker@duke.edu>
2023-03-03 11:14:31 -0500

    Found a few more spots where we had teamX instead of team14

--- client/src/test/java/edu/duke/ece651/team14/client/AppTest.java
+++ client/src/test/java/edu/duke/ece651/team14/client/AppTest.java
@@ -1,15 +1,13 @@
-package edu.duke.ece651.teamX.client;
+package edu.duke.ece651.team14.client;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
-  /**
-    @Test
-    void test_GetMessage() {
-      App a = new App();
-      assertEquals("Hello from the client for teamX", a.getMessage());
-    }
-  */
+  @Test
+  void test_GetMessage() {
+    App a = new App();
+    assertEquals("Hello from the client for team14", a.getMessage());
+  }
 }
--- server/src/test/java/edu/duke/ece651/team14/server/AppTest.java
+++ server/src/test/java/edu/duke/ece651/team14/server/AppTest.java
@@ -1,15 +1,14 @@
-package edu.duke.ece651.teamX.server;
+package edu.duke.ece651.team14.server;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Disabled
+import  org.junit.jupiter.api.Disabled;
 
 class AppTest {
-    @Test
-    @Disabled
-    void test_GetMessage() {
-      App a = new App();
-      assertEquals("Hello from the server for teamX", a.getMessage());
-    }
+  @Test
+  void test_GetMessage() {
+    App a = new App();
+    assertEquals("Hello from the server for team14", a.getMessage());
+  }
 }
 
