13d0fcb0d954d18ddaca0a761116ea9274425081
Jack Parker <jack.parker@duke.edu>
2023-03-27 18:57:19 -0400

    Completed tasks 1.1 through 1.4

--- shared/src/main/java/edu/duke/ece651/team14/shared/Player.java
+++ shared/src/main/java/edu/duke/ece651/team14/shared/Player.java
@@ -7,13 +7,18 @@ import java.util.HashSet;
 /**
  * A class to represent a player in the RISC game
  */
-public abstract class Player implements Serializable{
+public abstract class Player implements Serializable {
   // Player color
   private final Color color;
-
   // Player name
   private final String name;
-  
+  // Number of food resources the player currently has
+  private int foodAmt;
+  // Number of tech resources the player currently has
+  private int techAmt;
+  // Current max tech level of the player
+  private int maxTechLevel;
+
   /**
    * Creates a Player object from given color
    *
@@ -22,6 +27,9 @@ public abstract class Player implements Serializable{
   public Player(Color color, String name) {
     this.color = color;
     this.name = name;
+    this.foodAmt = 0;
+    this.techAmt = 0;
+    this.maxTechLevel = 1;
   }
 
   /**
@@ -53,7 +61,7 @@ public abstract class Player implements Serializable{
     }
     return true;
   }
-  
+
   /**
    * Returns the player color
    *
@@ -79,6 +87,56 @@ public abstract class Player implements Serializable{
    */
   public String getName() {
     return name;
+  }
+
+  public void addFoodResources(int toAdd) {
+    if (toAdd < 0) {
+      throw new IllegalArgumentException("Can't add a negative number of resources");
+    }
+    foodAmt += toAdd;
+  }
+
+  public void useFoodResources(int toUse) {
+    if (toUse < 0) {
+      throw new IllegalArgumentException("Can't use a negative number of resources");
+    }
+    if (toUse > foodAmt) {
+      throw new IllegalArgumentException("Not enough food resources");
+    }
+    foodAmt -= toUse;
+  }
+
+  public void addTechResources(int toAdd) {
+    if (toAdd < 0) {
+      throw new IllegalArgumentException("Can't add a negative number of resources");
+    }
+    techAmt += toAdd;
+  }
+
+  public void useTechResources(int toUse) {
+    if (toUse < 0) {
+      throw new IllegalArgumentException("Can't use a negative number of resources");
+    }
+    if (toUse > techAmt) {
+      throw new IllegalArgumentException("Not enough tech resources");
+    }
+    techAmt -= toUse;
+  }
+
+  public void increaseMaxTechLevel() {
+    maxTechLevel += 1;
+  }
+
+  public int getFoodAmt() {
+    return foodAmt;
+  }
+
+  public int getTechAmt() {
+    return techAmt;
+  }
+
+  public int getMaxTechLevel() {
+    return maxTechLevel;
   }
 
   @Override
